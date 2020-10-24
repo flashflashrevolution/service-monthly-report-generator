8d491382126ea77550cb2a8192c92270be2699ca
Zageron <hello@adambryant.ca>
2020-10-23 22:24:38 -0700

    Integrating typeorm and cleaning up constants.

--- src/index.ts
+++ src/index.ts
@@ -4,16 +4,102 @@ ImportEnvironmentVariables();
 import Process from "process";
 import mongoose from "mongoose";
 import { MonthlyReport } from "./MonthlyPatreonReport";
+import * as OAuth from 'simple-oauth2';
+import * as TypeORM from 'typeorm';
+import { CreatePatreonTokenFromOAuthToken } from "patreon-ts/dist/types";
+import { Entities } from "@flashflashrevolution/database-entities";
 
-const mongoUser: string = process.env.MONGO_USER as string;
-const mongoPass: string = process.env.MONGO_PASS as string;
-const mongoUri: string = process.env.MONGO_URI as string;
-const mongoDatabase: string = process.env.MONGO_DATABASE as string;
-
-async function Connect()
+const mongoConstants =
 {
-    const opts = { useNewUrlParser: true, connectTimeoutMS: 1000, poolSize: 10, useFindAndModify: false, useCreateIndex: true, useUnifiedTopology: true };
-    const connectionString = `mongodb+srv://${mongoUser}:${mongoPass}@${mongoUri}/${mongoDatabase}?retryWrites=true&w=majority`;
+    username: process.env.MONGO_USER as string,
+    password: process.env.MONGO_PASS as string,
+    uri: process.env.MONGO_URI as string,
+    databaseName: process.env.MONGO_DATABASE as string,
+    retryWrites: "true",
+    writes: "majority",
+};
+
+const sqlPatreonConstants =
+{
+    host: process.env.DB_HOST as string,
+    dbName: process.env.DB_PATREON as string,
+    dbUser: process.env.DB_PATREON_USER as string,
+    dbPass: process.env.DB_PATREON_PASS as string,
+};
+
+const connectionOptions: TypeORM.ConnectionOptions =
+{
+    name: sqlPatreonConstants.dbName,
+    type: "mysql",
+    host: sqlPatreonConstants.host,
+    port: 3306,
+    username: sqlPatreonConstants.dbUser,
+    password: sqlPatreonConstants.dbPass,
+    database: sqlPatreonConstants.dbName,
+    entities: [Entities.PatreonLink]
+};
+
+const patreonConstants =
+{
+    clientId: process.env.PATREON_CLIENT_ID as string,
+    clientSecret: process.env.PATREON_CLIENT_SECRET as string,
+    campaignId: process.env.PATREON_CAMPAIGN_ID as string,
+};
+
+const patreonOAuthConstants =
+{
+    host: "https://www.patreon.com",
+    tokenPath: "/api/oauth2/token",
+    authorizePath: "/oauth2/authorize",
+    scopes: "identity campaigns identity.memberships campaigns.members",
+};
+
+const patreonOAuthCredentials: OAuth.ModuleOptions =
+{
+    client:
+    {
+        id: patreonConstants.clientId,
+        secret: patreonConstants.clientSecret
+    },
+    auth:
+    {
+        tokenHost: patreonOAuthConstants.host,
+        tokenPath: patreonOAuthConstants.tokenPath,
+        authorizePath: patreonOAuthConstants.authorizePath
+    }
+};
+
+const client: OAuth.AuthorizationCode = new OAuth.AuthorizationCode(patreonOAuthCredentials);
+
+// Get the access token for the campaign. Set as the primary access token.
+// Set a timer for checking the validity of the token and refresh token.
+//  When necessary, refresh the access token and update the database.
+//  
+//let accessToken: OAuth.AccessToken = client.createToken(JSON.parse(accessTokenJSONString));
+
+
+
+async function ConnectToMongoDB()
+{
+    const opts =
+    {
+        useNewUrlParser: true,
+        connectTimeoutMS: 1000,
+        poolSize: 10,
+        useFindAndModify: false,
+        useCreateIndex: true,
+        useUnifiedTopology: true
+    };
+
+    const connectionString =
+        `mongodb+srv:\
+        //${mongoConstants.username}\
+        :${mongoConstants.password}\
+        @${mongoConstants.uri}\
+        /${mongoConstants.databaseName}\
+        ?retryWrites=${mongoConstants.retryWrites}\
+        &w=${mongoConstants.writes}`;
+
     await mongoose.connect(connectionString, opts);
 }
 
@@ -27,7 +113,7 @@ async function BuildTestModel()
 
 async function DoWork()
 {
-    await Connect();
+    await ConnectToMongoDB();
     await BuildTestModel();
     Process.exit(0);
 }
